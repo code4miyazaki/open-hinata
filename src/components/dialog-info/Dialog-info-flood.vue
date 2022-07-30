a266125ebce0386e541387553bc3c7246808c09e
kenotiai <kenotiai@gmail.com>
2022-07-30 23:16:40 +0900

    no message

--- src/components/dialog-info/Dialog-info-flood.vue
+++ src/components/dialog-info/Dialog-info-flood.vue
@@ -2,7 +2,7 @@
     <div>
         <!--海面上昇シミュレーション5m-->
         <div v-if="item.component.name === 'flood5m'">
-            <div class="info-content-div">
+            <div class="content-div">
                 <p v-html="item.title"></p><hr>
                 <input type="range" min="0" :max="floodMax5m" :step="seaLevelStep5m" class="flood-range5m" v-model.number="s_seaLevel5m" @input="flood5m" />
                 <div style="text-align: center;">{{ s_seaLevel5m.toFixed(1) }}m上昇した場合</div>
@@ -19,7 +19,7 @@
         </div>
         <!--海面上昇シミュレーション10m-->
         <div v-else-if="item.component.name === 'flood10m'">
-            <div class="info-content-div">
+            <div class="content-div">
                 <p v-html="item.title"></p><hr>
                 <input type="range" min="0" :max="floodMax10m" :step="seaLevelStep10m" class="flood-range10m" v-model.number="s_seaLevel10m" @input="flood10m" />
                 <div style="text-align: center;">{{ s_seaLevel10m.toFixed(1) }}m上昇した場合</div>
@@ -217,5 +217,7 @@
 </script>
 
 <style scoped>
-
+.content-div{
+  width: 200px;
+}
 </style>
